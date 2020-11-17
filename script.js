db1561d73148cadfd2579d801324b48d006ead4b
fahrizaall <68831428+fahrizaall@users.noreply.github.com>
2020-11-17 17:09:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -157,7 +157,7 @@ function hide(){
 window.onload=function(){
   var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
   if (mobile) {
-      alert("Visit this on a Computer for Better View");              
+      alert("Visit this on a pc/laptop for better view");              
   } else {
 
   }
