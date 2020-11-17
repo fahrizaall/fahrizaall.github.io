ece895ec4399027046e06c55810b3f79dbc0a6ab
fahrizaall <68831428+fahrizaall@users.noreply.github.com>
2020-11-17 17:06:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -152,3 +152,13 @@ function hide(){
   let display = document.querySelector('.owner-popup');
   display.style.display = 'none';
 }
+
+
+window.onload=function(){
+  var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+  if (mobile) {
+      alert("Visit this on a Computer for Better View");              
+  } else {
+
+  }
+}
